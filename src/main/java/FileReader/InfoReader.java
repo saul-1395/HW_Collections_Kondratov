bab7c193cde7e4e63229518a02c41e9fcdc40fa8
Denis Kondratov <zel-1395@yandex.ru>
2021-02-04 17:46:40 +0300

    commit 04.02.2021

--- src/main/java/FileReader/InfoReader.java
+++ src/main/java/FileReader/InfoReader.java
@@ -3,33 +3,40 @@ package FileReader;
 import Converter.WordCounter;
 
 import java.io.File;
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class InfoReader {
     private InputData inputData;
     private WordCounter wordCounter;
     private String path;
+    private ArrayList<String> dataList = new ArrayList<>();
 
     public void appStart() {
+
         inputPath();
         inputData = new InputData();
-        wordCounter = new WordCounter(inputData.getStringList(path));
+        dataList = inputData.getStringList(path);
+        wordCounter = new WordCounter(dataList);
+
         System.out.println("Количество слов в тексте");
         System.out.println(wordCounter.getResult());
+
         System.out.println("Чаще всего встречается");
         System.out.println(wordCounter.getMaxLenght().toString());
+
         System.out.println("По алфавиту");
         System.out.println(wordCounter.alphabeticalOrder());
-
-
     }
 
     private void inputPath() {
+
         System.out.println("Введите полный путь или имя файлa");
+
         String resourcesPath = "src/main/resources/";
 
-
         Scanner in = new Scanner(System.in);
+
         String inputPath = in.nextLine();
 
         if (!inputPath.isEmpty()) {
